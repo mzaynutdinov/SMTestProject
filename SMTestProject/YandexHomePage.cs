803859655da4fdaf95619acc809c472754b7fc95
mzaynutdinov <zaynutdinov.maxim@yandex.ru>
2019-03-18 10:31:10 +0300

    Оптимизация using

--- SMTestProject/YandexHomePage.cs
+++ SMTestProject/YandexHomePage.cs
@@ -1,11 +1,5 @@
-﻿using NUnit.Framework;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using SeleniumExtras.PageObjects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SCTestApp.Pages
 {
--- SMTestProject/YandexRaspPage.cs
+++ SMTestProject/YandexRaspPage.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.PageObjects;
